7927ffb728f37bdd760921a2ecc489414e6ce89e
firefood <65716578+foodwithfire@users.noreply.github.com>
2023-04-11 16:16:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import pygame, time, os, sys, cmd, random, keyboard
 
 # screen vars
-screen_size = (1200, 800)
+screen_size = (800, 600)
 screen = pygame.display.set_mode(screen_size)
 
 # player vars
 player_size = (50, 50)
 player_pos = [0, 0]
-player_speed = 0.1
+player_speed = 1/500
+player_velocity_x = 0
+player_velocity_y = 0
 
 # setting vars
-controls = {"up": "z",
-            "left": "q",
+controls = {"up": "w",
+            "left": "a",
             "down": "s",
             "right": "d"}
 clock = pygame.time.Clock()
 delta_time = clock.tick(60)
+air_resistance = 0.98
 
 running = True
 while running:
@@ -25,8 +28,16 @@ while running:
     # get inputs and update the player position
     player_direction = (keyboard.is_pressed(controls["right"]) - keyboard.is_pressed(controls["left"]),
                         keyboard.is_pressed(controls["up"]) - keyboard.is_pressed(controls["down"]))
-    player_pos[0] += player_direction[0] * player_speed * delta_time
-    player_pos[1] -= player_direction[1] * player_speed * delta_time
+
+    # update player's velocity
+    player_velocity_x += player_direction[0] * player_speed
+    player_velocity_y += player_direction[1] * player_speed
+    player_velocity_x *= air_resistance
+    player_velocity_y *= air_resistance
+
+    # update player's position
+    player_pos[0] += player_velocity_x * delta_time
+    player_pos[1] -= player_velocity_y * delta_time
 
     # create the surface of the player and fill it with white
     player = pygame.Surface(player_size)
