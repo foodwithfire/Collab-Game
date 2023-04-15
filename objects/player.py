175bdd1965d392608ef8ad2721dacb953d91035c
YouLeGamer <125671158+YouLeGamer@users.noreply.github.com>
2023-04-15 17:44:16 +0200

    Update player.py

--- objects/player.py
+++ objects/player.py
@@ -1,6 +1,7 @@
 class Player:
 
-    def __init__(self, player_health, player_damage, mana, player_level, placeholder, player_pos, player_weapon):
+    def __init__(self, player_name, player_health, player_damage, mana, player_level, placeholder, player_pos, player_weapon):
+        self.player_name = player_name
         self.player_health = player_health
         self.player_damage = player_damage
         self.mana = mana
@@ -8,4 +9,25 @@ class Player:
         self.player_inventory = placeholder  # Awaiting to create the inventory class which will consist of a dictionary
         self.player_pos = player_pos
         self.player_weapon = player_weapon
-        
+
+    def get_player_name(self):
+        return self.player_name
+
+    def get_player_health(self):
+        return self.player_health
+
+    def get_player_damage(self):
+        return self.player_damage
+
+    def get_mana(self):
+        return self.mana
+
+    def get_player_level(self):
+        return self.player_level
+
+    def get_player_pos(self):
+        return self.player_pos
+
+    def get_player_weapon(self):
+        return self.player_weapon
+    
