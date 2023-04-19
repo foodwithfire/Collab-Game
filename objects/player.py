b522012d5cea87058feedaf6d4613db152d99bdf
firefood <65716578+foodwithfire@users.noreply.github.com>
2023-04-19 17:01:18 +0200

    Update player.py

--- objects/player.py
+++ objects/player.py
@@ -1,33 +1,16 @@
+import pygame
+
+
 class Player:
 
     def __init__(self, player_name, player_health, player_damage, mana, player_level, placeholder, player_pos, player_weapon):
-        self.player_name = player_name
-        self.player_health = player_health
-        self.player_damage = player_damage
+        self.img_path = "assets/textures/player.png"
+        self.surface = pygame.image.load(self.img_path)
+        self.name = player_name
+        self.health = player_health
+        self.damage = player_damage
         self.mana = mana
-        self.player_level = player_level
-        self.player_inventory = placeholder  # Awaiting to create the inventory class which will consist of a dictionary
-        self.player_pos = player_pos
-        self.player_weapon = player_weapon
-
-    def get_player_name(self):
-        return self.player_name
-
-    def get_player_health(self):
-        return self.player_health
-
-    def get_player_damage(self):
-        return self.player_damage
-
-    def get_mana(self):
-        return self.mana
-
-    def get_player_level(self):
-        return self.player_level
-
-    def get_player_pos(self):
-        return self.player_pos
-
-    def get_player_weapon(self):
-        return self.player_weapon
-    
+        self.level = player_level
+        self.inventory = placeholder  # Awaiting to create the inventory class which will consist of a dictionary
+        self.pos = player_pos
+        self.weapon = player_weapon
