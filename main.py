01b44957deb631fb681383ad0ddc6d68144013ed
firefood <65716578+foodwithfire@users.noreply.github.com>
2023-04-19 16:49:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-##### IMPORTS #####
+# IMPORTS --------------------------------------------------------------------------------------------------------------
 
 import pygame
 import time
@@ -10,7 +10,7 @@ import keyboard
 import scripts.random_things as rt  # Not used for now
 from objects.player import Player  # Not used for now
 
-##### VARIABLES SETTING-UP #####
+# VARIABLES SETTING-UP -------------------------------------------------------------------------------------------------
 
 # Screen variables: size, color; sets "screen" as a screen display
 screen_size = (800, 600)
@@ -28,13 +28,12 @@ if wall_density != 0:
             if random.randint(0, 100) < wall_density:
                 walls.append((pygame.Surface(wall_size), x * wall_size[0], y * wall_size[0]))
 
-# Player variables: size, color, position, speed, velocity in x and y
+# Player variables: size, color, position, speed, velocity
 player_size = (50, 50)
 player_color = (255, 255, 255)
 player_pos = [350, 250]
 player_speed = 1/1000
-player_velocity_x = 0
-player_velocity_y = 0
+player_velocity = [0, 0]
 
 # Other variables: controls, clock, delta time and air resistance
 controls = {"up": "w",
@@ -47,7 +46,7 @@ air_resistance = 0.99
 
 running = True
 
-##### GAME LOOP #####
+# GAME LOOP ------------------------------------------------------------------------------------------------------------
 
 while running:
     # Fills screen with black (the color wanted in "screen variables")
@@ -58,14 +57,14 @@ while running:
                         keyboard.is_pressed(controls["up"]) - keyboard.is_pressed(controls["down"]))
 
     # Updates player's velocity variables
-    player_velocity_x += player_direction[0] * player_speed
-    player_velocity_y += player_direction[1] * player_speed
-    player_velocity_x *= air_resistance
-    player_velocity_y *= air_resistance
+    player_velocity[0] += player_direction[0] * player_speed
+    player_velocity[1] += player_direction[1] * player_speed
+    player_velocity[0] *= air_resistance
+    player_velocity[1] *= air_resistance
 
     # Updates variables of position
-    player_pos[0] += player_velocity_x * delta_time
-    player_pos[1] -= player_velocity_y * delta_time
+    player_pos[0] += player_velocity[0] * delta_time
+    player_pos[1] -= player_velocity[1] * delta_time
 
     # Creates the "player": a white square of the color and size chosen in player variables
     player = pygame.Surface(player_size)
