d961acdb9e7a49ef2934bc04a8e5dda01f9705eb
firefood <65716578+foodwithfire@users.noreply.github.com>
2023-04-21 13:10:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,48 +30,24 @@ if wall_density != 0:
                 walls.append((pygame.Surface(wall_size), x * wall_size[0], y * wall_size[0]))
 
 # Player
-player = Player("firefood", 0, 0, 0, 0, 0, 0, 0)
-
-# Other variables: controls, clock, delta time and air resistance
-controls = {"up": "w",
-            "left": "a",
-            "down": "s",
-            "right": "d"}
-clock = pygame.time.Clock()
-delta_time = clock.tick(60)
-air_resistance = 0.99
-
-running = True
+player = Player(screen, "firefood", 0, 0, 0, 0, 0, 0)
+# Button
+button1 = Button((150, 100), (200, 100), screen)
 
 # GAME LOOP ------------------------------------------------------------------------------------------------------------
-button1 = Button((150, 100), (200, 100), screen)
+running = True
 while running:
     # Fills screen with black (the color wanted in "screen variables")
     screen.fill(screen_color)
-
-    button1.update()
-
-    # Gets inputs from the keyboard and updates player's position
-    player_direction = (keyboard.is_pressed(controls["right"]) - keyboard.is_pressed(controls["left"]),
-                        keyboard.is_pressed(controls["up"]) - keyboard.is_pressed(controls["down"]))
-
-    # Updates player's velocity variables
-    player.velocity[0] += player_direction[0] * player.speed
-    player.velocity[1] += player_direction[1] * player.speed
-    player.velocity[0] *= air_resistance
-    player.velocity[1] *= air_resistance
-
-    # Updates variables of position
-    player.pos[0] += player.velocity[0] * delta_time
-    player.pos[1] -= player.velocity[1] * delta_time
 
     # Displays obstacles
     for wall in walls:
         wall[0].fill(wall_color)
         screen.blit(wall[0], (wall[1], wall[2]))
-    screen.blit(player.surface, player.pos)
 
     # Updates the screen
+    player.update()
+    button1.update()
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
