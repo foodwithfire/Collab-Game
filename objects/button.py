190078e2f2600fe8d10d987f4c09dd24cd4e9a33
firefood <65716578+foodwithfire@users.noreply.github.com>
2023-04-21 13:16:36 +0200

    Update button.py

--- objects/button.py
+++ objects/button.py
@@ -1,22 +1,19 @@
 import pygame
 
+
 class Button:
-    def __init__(self, pos, size, screen, text=None):
+    def __init__(self, path, pos, screen):
+        self.path = path
         self.pos = pos
-        self.size = size
         self.screen = screen
-        self.text = text
-        self.surface = pygame.Surface(self.size)
+        self.surface = pygame.image.load(self.path)
+        self.size = self.surface.get_size()
+        self.mouse_pos = None
 
     def update(self):
         self.mouse_pos = pygame.mouse.get_pos()
         if (self.mouse_pos[0] > self.pos[0] and self.mouse_pos[0] < (self.pos[1] + self.size[0])) and\
            (self.mouse_pos[1] > self.pos[1] and self.mouse_pos[1] < (self.pos[1] + self.size[1])):
             if pygame.mouse.get_pressed()[0]:
-                self.color = (255, 255, 255)
-            else:
-                self.color = (170, 170, 170)
-        else:
-            self.color = (70, 70, 70)
-        self.surface.fill(self.color)
+                print("pressed")
         self.screen.blit(self.surface, self.pos)
