afa43f09850da27b914e2c7130a15df5cae479b0
YouLeGamer <yoann.vandomme@outlook.fr>
2023-04-22 11:43:36 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,3 +2,10 @@ controls = {"up": "z",
             "left": "q",
             "down": "s",
             "right": "d"}
+
+lang = "English"
+
+languages = {
+    "English": "en_us",
+    "French": "fr_fr"
+}
