92eecb00a7ac2af10593a2b342d65e3c1c2ac11a
YouLeGamer <yoann.vandomme@outlook.fr>
2023-04-23 18:49:35 +0200

    Added entity and npc super classes

---
+++ classes/entity.py
@@ -0,0 +1,8 @@
+class Entity:
+
+    def __init__(self, name, health, damage, hostile, texture):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.hostile = hostile
+        self.texture = texture
---
+++ classes/npc.py
@@ -0,0 +1,8 @@
+class Npc:
+
+    def __init__(self, name, health, hostile, speech, text):
+        self.name = name
+        self.health = health
+        self.hostile = hostile
+        self.speech = speech
+        self.text = text
